8971f2756b3a1ef0e1138ddfac05b5d055927c4a
Sky Jian <sky000@gmail.com>
2012-03-30 16:33:50 +0800

    change configration

--- dict/models.py
+++ dict/models.py
@@ -3,6 +3,8 @@ from django.db import models
 class Host(models.Model):
     name = models.CharField(max_length=128)
     ip = models.CharField(max_length=15)
+    ops_contacter = models.CharField(max_length=64)
+    os_info = models.CharField(max_length=64)
     gmt_created = models.DateTimeField('date published')
     gmt_modified = models.DateTimeField('date modified')
     comment = models.CharField(max_length=256)
@@ -10,6 +12,9 @@ class Host(models.Model):
 class Instance(models.Model):
     host_id = models.IntegerField()
     name = models.CharField(max_length=128)
+    dba_contacter = models.CharField(max_length=64)
+    version = models.DecimalField(max_digits=5, decimal_places=2)
+    version_comment = models.CharField(max_length=64)
     gmt_created = models.DateTimeField('date published')
     gmt_modified = models.DateTimeField('date modified')
     comment = models.CharField(max_length=256)
--- settings.py
+++ settings.py
@@ -12,7 +12,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'ops',                      # Or path to database file if using sqlite3.
+        'NAME': 'mygcs',                      # Or path to database file if using sqlite3.
         'USER': 'ops',                      # Not used with sqlite3.
         'PASSWORD': 'ops',                  # Not used with sqlite3.
         'HOST': '127.0.0.1',                      # Set to empty string for localhost. Not used with sqlite3.
